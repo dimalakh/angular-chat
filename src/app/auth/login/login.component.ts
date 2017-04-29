e248b2ad58d736bc73faa1800863d1ad2ee1b80a
dimalakh <dmytrolakh@gmail.com>
2017-04-29 23:40:58 +0300

    created test form for auth/login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -8,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 
 export class LoginComponent implements OnInit {
 
-  constructor() {
-
+  constructor(private http: Http) {
   }
+
 
   ngOnInit() {
 
